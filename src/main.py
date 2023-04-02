c6826e8ae89985a0ff752b46a9cabdf3d5385995
Robert Z <nebuchadenezzar@gmail.com>
2023-04-02 16:23:34 +0200

    Fix EOF

--- src/main.py
+++ src/main.py
@@ -50,4 +50,4 @@ if __name__ == "__main__":
     port = os.getenv("PORT")
     if not port:
         port = 8080
-    uvicorn.run(app, host="0.0.0.0", port=8080) 
+    uvicorn.run(app, host="0.0.0.0", port=8080)
